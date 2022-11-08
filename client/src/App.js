36158d6e5311ab19afdf2526dbdecc97605f9d46
Dany1718 <daniel.dossantos@ufl.edu>
2022-11-08 17:50:16 -0500

    backend and frontend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register"
 import Home from "./pages/home/Home"
+import {
+ BrowserRouter as Router,
+ Routes,
+  Route
+} from "react-router-dom";
 function App() {
-  return <Home/>;
+  return (
+  <Login/>
+  )
 }
 
 export default App;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css"
 
 export default function Home() {
   return (
     <>
       <Topbar />
-      <div className="homeContainer">  
+      <div className="homeContainer">
+         <Sidebar/>
       </div>
     </>
   );
