a0a576e22a86342d95f1afd31692a0323c482482
Tarun Dasari <tarund2302@gmail.com>
2022-12-06 22:16:23 -0500

    Commit

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,28 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
-    console.log("Connected to MongoDB!");
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log("Connected to MongoDB");
 });
 
-app.listen(8800, () => {
-    console.log("backend server is running!!");
-});
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+
+app.listen(3000, () => {
+    console.log("Backend server is running!");
+})
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,30 @@
 import "./register.css";
+import validator from 'validator';
+
+console.log("HELLO");
+
 
 export default function Register() {
+
+  function submitRegistration(){
+    //e.preventDefault();
+    console.log('Successfully Registered.');
+    
+  }
+
+  let validateEmail = (email) => {
+    if(validator.isEmail(email) && email.length > 0){
+      console.log("Email is valid");
+      return true;
+    }
+    console.log("Email is invalid");
+    return false;
+  }
+
+  console.log("YES");
+
   return (
+
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
@@ -13,10 +36,10 @@ export default function Register() {
         <div className="loginRight">
           <div className="loginBox">
             <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Email" type="email" className="loginInput" />
             <input placeholder="Password" className="loginInput" />
             <input placeholder="Confirm Password" className="loginInput" />
-            <button className="loginButton">Sign Up</button>
+            <button className="loginButton" onClick={submitRegistration}>Sign Up</button>
             <button className="loginRegisterButton">
               Already have an account?
             </button>
